6e403851cd14841918f4cfe04438c52012777f1b
Péter Ekler <peter.ekler@gmail.com>
2018-10-11 20:47:09 +0200

    Resource qualifier demos, layout demo with LinearLayout and RelativeLayout

---
+++ LayoutDemo/app/src/main/java/hu/aut/android/layoutdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package hu.aut.android.layoutdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_login)
+
+    }
+}
---
+++ ResourceQualifierDemo/app/src/main/java/hu/aut/android/resourcequalifierdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package hu.aut.android.resourcequalifierdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
